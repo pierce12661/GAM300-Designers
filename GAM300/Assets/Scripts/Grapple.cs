67335c3a97af09c583eed3eb49c2f16882969185
itshrnz <lylhenryy@gmail.com>
2023-08-31 17:38:42 +0800

    testing stuff

--- GAM300/Assets/Scripts/Grapple.cs
+++ GAM300/Assets/Scripts/Grapple.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Grapple : MonoBehaviour
 {
     [Header("References")]
-    private PlayerMovement pm;
+    //private PlayerMovement pm;
     public Transform cam;
     public Transform grappleStart;
     public LayerMask canBeGrappled;
@@ -23,13 +26,14 @@ public class Grapple : MonoBehaviour
 
     [Header("Input")]
     public KeyCode grappleKey = KeyCode.Mouse0;
+    public KeyCode grappleRemoveKey = KeyCode.Mouse1;
 
     private bool grappling;
 
     // Start is called before the first frame update
     void Start()
     {
-        pm = GetComponent<PlayerMovement>();
+        //pm = GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
@@ -38,6 +42,11 @@ public class Grapple : MonoBehaviour
         if (Input.GetKeyDown(grappleKey))
         {
             StartGrapple();
+        }
+
+        if (Input.GetKeyDown(grappleRemoveKey))
+        {
+            StopGrapple();
         }
 
         // if timer > 0, keep counting down
@@ -65,7 +74,7 @@ public class Grapple : MonoBehaviour
 
         grappling = true;
 
-        // shoot line renderer in straight line to where player is looking, from the camera
+        // shoot line renderer in straight line depending on where cam is facing
         RaycastHit hit;
         if (Physics.Raycast(cam.position, cam.forward, out hit, maxGrappleDistance, canBeGrappled))
         {
