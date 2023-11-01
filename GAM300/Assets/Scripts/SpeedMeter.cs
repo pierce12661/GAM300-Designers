65f3d4a1c56cd154cb01d0cb577abd14b5b6d5f7
AppoDaisies <xavier5867@hotmail.com>
2023-11-01 15:36:44 +0800

    Battery

--- GAM300/Assets/Scripts/SpeedMeter.cs
+++ GAM300/Assets/Scripts/SpeedMeter.cs
@@ -6,11 +6,13 @@ public class SpeedMeter : MonoBehaviour
 {
     [SerializeField] private KartController kc;
 
-    public Transform sliderFill;
+    public Transform overlay;
 
-    private Vector3 targetFill;
+    private Vector3 targetOverlay;
 
     public CanvasGroup cg;
+    public Transform keys;
+
 
     private float timer;
 
@@ -23,13 +25,15 @@ public class SpeedMeter : MonoBehaviour
 
     public void SpeedSlider()
     {
-        targetFill = new Vector3(kc.currentBattery / kc.maxBattery, sliderFill.localScale.y, sliderFill.localScale.z);
+        targetOverlay = new Vector3(1 - kc.currentBattery / kc.maxBattery, overlay.localScale.y, overlay.localScale.z);
 
-        sliderFill.localScale = Vector3.Lerp(sliderFill.localScale, targetFill, 8.0f * Time.deltaTime);
+        overlay.localScale = Vector3.Lerp(overlay.localScale, targetOverlay, 8.0f * Time.deltaTime);
     }
 
     public void ControlsPrompt()
     {
+        Vector3 target = new Vector3(0.007f, 0.007f, 0.007f);
+
         if(kc.realSpeed < 2.0f && kc.acceleration == 0 && kc.steerAmount == 0 && !TransitionManager.instance.isGameOver)
         {
             timer += 1.0f * Time.deltaTime;
@@ -43,13 +47,18 @@ public class SpeedMeter : MonoBehaviour
         {
             timer = 6.0f;
 
-            cg.alpha = Mathf.Lerp(cg.alpha, 1, 3.0f * Time.deltaTime);
+            //cg.alpha = Mathf.Lerp(cg.alpha, 1, 3.0f * Time.deltaTime);
+            cg.gameObject.transform.localScale = Vector3.Lerp(cg.transform.localScale, target, 7.0f * Time.deltaTime);
+            //keys.localScale = Vector3.Lerp(keys.localScale, Vector3.one, 3.0f * Time.deltaTime);
         }
         else if(timer < 3.0f)
         {
-            cg.alpha = Mathf.Lerp(cg.alpha, 0, 6.0f * Time.deltaTime);
+            //cg.alpha = Mathf.Lerp(cg.alpha, 0, 6.0f * Time.deltaTime);
 
-            if(timer <= 0)
+            cg.gameObject.transform.localScale = Vector3.Lerp(cg.transform.localScale, Vector3.zero, 12.0f * Time.deltaTime);
+            //keys.localScale = Vector3.Lerp(keys.localScale, Vector3.zero, 3.0f * Time.deltaTime);
+
+            if (timer <= 0)
             {
                 timer = 0;
             }
